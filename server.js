db34479345e54f8830c35318628bbda8d3f41246
Satriawan Kadek <ikadek.satriawan@gmail.com>
2026-07-11 18:06:45 +0800

    fixing: prod

--- server.js
+++ server.js
@@ -215,11 +215,4 @@ ${articleText}
     }
 });
 
-const PORT =
-    process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(
-        `🚀 Server berjalan di http://localhost:${PORT}`
-    );
-});
+export default app;
